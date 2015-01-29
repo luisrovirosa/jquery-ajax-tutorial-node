cda2b6f66a05ea6271c113e3563e855d24dadd83
Luis Rovirosa <luisrovirosa@gmail.com>
2015-01-29 10:39:58 +0100

    Fixed the / information

--- index.js
+++ index.js
@@ -12,11 +12,9 @@ app.set('port', (process.env.PORT || 5000));
 app.get('/', function (request, response, next) {
     var EOF = "<br />";
     var explanation = "The methods are:" + EOF +
-        "GET <a href='/email/new'>/email/new</a>&nbsp;&nbsp;&nbsp;&nbsp;-> To check if there is new email" + EOF +
-        "GET <a href='/email/1234'>/email/:id</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-> To retrieve the email information" + EOF +
-        "POST /email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-> To send an email" + EOF + EOF + EOF +
-        "The existing email id are: 1234, 2345, 3456 and 4567. If you retrieve a different id it will be generated randomly";
-
+        "GET <a href='/hello/luis'>/hello/:name</a>&nbsp;&nbsp;&nbsp;&nbsp;-> To say hello" + EOF +
+        "GET <a href='/exchange/?amount=100&currency=euro'>/exchange/</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-> To check the currency conversion" + EOF +
+        "GET <a href='/sleep'>/sleep</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-> To sleep some time and get the response or an error" + EOF;
     response.send(explanation);
 });
 
